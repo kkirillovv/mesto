7f3351bafb9c87abcb719be1744197c96f12ded0
Kirill Kirillov <admin@sopka.net>
2023-04-01 21:43:09 +0300

    Пятый спринт, версия 2.3

--- js/index.js
+++ js/index.js
@@ -1,139 +1,107 @@
-const initialCards = [
-  {
-    name: 'Вулкан Тятя',
-    link: './images/photo-tyatya.jpg',
-    alt: 'Стратовулкан Тятя (1819 метров) типа сомма-везувий («вулкан в вулкане») на острове Кунашир. «Отец-гора»'
-  },
-  {
-    name: 'Вулкан Эбе́ко',
-    link: './images/photo-ebeko.jpg',
-    alt: 'Действующий вулкан Эбе́ко высотой 1156 метров в 6 км от Северо-Курильска на острове Парамушир'
-  },
-  {
-    name: 'Вулкан Крени́цына',
-    link: './images/photo-krinicina.jpg',
-    alt: 'Действующий классический сомма вулкан Крени́цына в южной части острова Онекотан высотой 1324 метра'
-  },
-  {
-    name: 'Вулкан Баранского',
-    link: './images/photo-baranskogo.jpg',
-    alt: 'Действующий вулкан Баранского на острове Итуруп, Курильский архипелаг'
-  },
-  {
-    name: 'Вулкан Ала́ид',
-    link: './images/photo-alaid.jpg',
-    alt: 'Самый северный и самый высокий вулкан Курильских островов Ала́ид, действующий на острове Атласова (2285 метров)'
-  },
-  {
-    name: 'Вулкан Чикура́чки',
-    link: './images/photo-chikurachki.jpg',
-    alt: 'Действующий стратовулкан с вершинным кратером Чикура́чки высотой 1816 метров на острове Парамушир Большой Курильской гряды'
-  }
-];
-
-function makeCard (item) {
-  // клонируем содержимое тега template
-  const element = cardTemplate.querySelector('.element').cloneNode(true);
-  // заполняем карточку
-  element.querySelector('.element__title').textContent = item.name;
-  element.querySelector('.element__photo').src = item.link;
-  element.querySelector('.element__photo').alt = item.alt;
-  // отображаем на странице
-  elements.prepend(element);
-}
+// A. Объявляем переменные -------------------------------------------------
 
 const cardTemplate = document.querySelector('#article_card').content;
 const elements = document.querySelector('.elements');
 
-// 1. Шесть карточек «из коробки»
-initialCards.forEach(function (item) {
-  makeCard(item)
-});
+const content = document.querySelector('.content');
+const buttonEditProfile = content.querySelector('.profile-info__edit');
+const buttonAddPhoto = content.querySelector('.profile__add-photo');
+const profileName = content.querySelector('.profile-info__name');
+const profileActivity = content.querySelector('.profile-info__activity');
+// Попап формы редактирования
+const popupEditForm = document.querySelector('#edit-profile');
+const buttonClosePopupEditForm = popupEditForm.querySelector('.popup__close');
+// Попап формы добавления фотографии
+const popupAddPhoto = document.querySelector('#add-photo');
+const buttonClosePopupAddPhoto = popupAddPhoto.querySelector('.popup__close');
+// Попап показа фотографий
+const popupShowPhoto = document.querySelector('#show');
+const buttonClosePopupShowPhoto = popupShowPhoto.querySelector('.popup__close');
 
-function handleClickElement(e) { 
+// B. Объявляем функции ----------------------------------------------------
 
-// 4. Лайк карточки надо сделать через toggle
-  if (e.target.classList.contains("element__like")) {
-    e.target.classList.toggle("element__like_active");
-  }
+function createCard (item) {
+  // клонируем содержимое тега template
+  const element = cardTemplate.querySelector('.element').cloneNode(true);
+  const likeElement = element.querySelector('.element__like');
+  const trashElement = element.querySelector('.element__trash');
+  const photoElement = element.querySelector('.element__photo');
+  const titleElement = element.querySelector('.element__title');
+  const titleCardDetails = popupShowPhoto.querySelector('.card-details__title');
+  const imageCardDetails = popupShowPhoto.querySelector('.card-details__image')
 
-// 5. Удаление карточки
-  if (e.target.classList.contains("element__trash")) {
-    e.target.parentElement.remove();
-  }
+  // заполняем карточку
+  titleElement.textContent = item.name;
+  photoElement.src = item.link;
+  photoElement.alt = item.alt;
 
-// 6. Открытие попапа с картинкой
-  if (e.target.classList.contains("element__photo")) {
-    let title = e.target.nextElementSibling.nextElementSibling.firstElementChild;
-    popupShow.querySelector('.card-details__title').textContent = title.textContent;
-    popupShow.querySelector('.card-details__image').src = e.target.src;
-    popupShow.querySelector('.card-details__image').alt = e.target.alt;
-    popupShow.classList.toggle("popup_opened");
-  }
+  // 4. Лайк карточки надо сделать через toggle / Слушатель события
+  likeElement.addEventListener('click', function () {
+    likeElement.classList.toggle("element__like_active");
+  });
+  // 5. Удаление карточки / Слушатель события
+  trashElement.addEventListener('click', function () {
+    trashElement.parentElement.remove();
+  });
+  // 6. Открытие попапа с картинкой / Слушатель события
+  photoElement.addEventListener('click', function () {
+    titleCardDetails.textContent = titleElement.textContent;
+    imageCardDetails.src = photoElement.src;
+    imageCardDetails.alt = photoElement.alt;
+    togglePopup(popupShowPhoto);
+  });
+  return element;
 }
 
-elements.addEventListener('click', handleClickElement);
-
-let content = document.querySelector('.content');
-let editButton = content.querySelector('.profile-info__edit');
-let addButton = content.querySelector('.profile__add-photo');
-let profileName = content.querySelector('.profile-info__name');
-let profileActivity = content.querySelector('.profile-info__activity');
-let popupEdit = document.querySelector('#form');
-let popupContainer = popupEdit.querySelector('.popup__container');
-let closeButtonPopupEdit = popupEdit.querySelector('.popup__close');
-let popupShow = document.querySelector('#show');
-let closeButtonPopupShow = popupShow.querySelector('.popup__close');
-
-function togglePopupEdit() {
-  popupEdit.classList.toggle("popup_opened");
+function renderCard (item) { 
+  // отображаем на странице
+  elements.prepend(createCard(item));
 }
 
-function togglePopupShow() {
-  popupShow.classList.toggle("popup_opened");
+function togglePopup(item) {
+  item.classList.toggle("popup_opened");
 }
 
-function handleFormSubmit (evt) {
+function editProfileInfo (evt) {
+  const textInputName = popupEditForm.querySelector('.edit-form__input-text_type_name');
+  const textInputActivity = popupEditForm.querySelector('.edit-form__input-text_type_activity');  
+
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  profileName.textContent = popupEdit.querySelector('.edit-form__input-text_type_name').value;
-  profileActivity.textContent = popupEdit.querySelector('.edit-form__input-text_type_activity').value;
-  togglePopupEdit();
+  profileName.textContent = textInputName.value;
+  profileActivity.textContent = textInputActivity.value;
+  togglePopup(popupEditForm);
 }
 
 // 3. Добавление карточки
 function makeNewCard (evt) {
+  const photoTitle = popupAddPhoto.querySelector('.edit-form__input-text_type_title');
+  const photoLink = popupAddPhoto.querySelector('.edit-form__input-text_type_link');
+
   evt.preventDefault();
-  makeCard({
-    name: popupEdit.querySelector('.edit-form__input-text_type_title').value,
-    link: popupEdit.querySelector('.edit-form__input-text_type_link').value,
-    alt: popupEdit.querySelector('.edit-form__input-text_type_title').value
+  renderCard({
+    name: photoTitle.value,
+    link: photoLink.value,
+    alt: photoTitle.value
   }); 
-  togglePopupEdit();
+  togglePopup(popupAddPhoto);
 }
 
-function creatFormPopup(id, data) {
-  const cardTemplate = document.querySelector(id).content;
-  const form = cardTemplate.querySelector('.edit-form').cloneNode(true);
-  const formElement = popupEdit.querySelector('.edit-form');
-
+function creatFormPopup(form, data) {
   data.forEach(function (item) {
     form.querySelector(item.selector).value = item.value;
   });
-
-  formElement.remove();
-  popupContainer.append(form);
-  switch (id) {
-    case "#edit-profile":
-      form.addEventListener('submit', handleFormSubmit);
+  switch (form.id) {
+    case "edit-profile":
+      form.addEventListener('submit', editProfileInfo);
       break;
-    case "#add-photo":
+    case "add-photo":
       form.addEventListener('submit', makeNewCard);
       break;
   }
 }
 
 function editInfo () {
-  let inputTextFields = [
+  const inputTextFields = [
     {
       selector: '.edit-form__input-text_type_name',
       value: profileName.textContent,
@@ -143,13 +111,13 @@ function editInfo () {
       value: profileActivity.textContent,
     }
   ];
-  creatFormPopup('#edit-profile', inputTextFields);
-  togglePopupEdit();
+  creatFormPopup(popupEditForm, inputTextFields);
+  togglePopup(popupEditForm);
 }
 
 // 2. Форма добавления карточки
 function addPhoto () {
-  let inputTextFields = [
+  const inputTextFields = [
     {
       selector: '.edit-form__input-text_type_title',
       value: '',
@@ -159,11 +127,26 @@ function addPhoto () {
       value: '',
     }
   ];
-  creatFormPopup('#add-photo', inputTextFields);
-  togglePopupEdit();
+  creatFormPopup(popupAddPhoto, inputTextFields);
+  togglePopup(popupAddPhoto);
 }
 
-editButton.addEventListener('click', editInfo);
-addButton.addEventListener('click', addPhoto);
-closeButtonPopupEdit.addEventListener('click', togglePopupEdit);
-closeButtonPopupShow.addEventListener('click', togglePopupShow);
+// C. Реализуем добавление обработчиков ------------------------------------
+
+// 1. Шесть карточек «из коробки»
+initialCards.forEach(function (item) {
+  renderCard(item)
+});
+
+// Сниферы 
+buttonEditProfile.addEventListener('click', editInfo);
+buttonAddPhoto.addEventListener('click', addPhoto);
+buttonClosePopupEditForm.addEventListener('click', function () {
+  togglePopup(popupEditForm);
+});
+buttonClosePopupAddPhoto.addEventListener('click', function () {
+  togglePopup(popupAddPhoto);
+});
+buttonClosePopupShowPhoto.addEventListener('click', function () {
+  togglePopup(popupShowPhoto);
+});
